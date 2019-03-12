7d6fff03fd17bc5c03cef12c2583218b0f3c0e32
Zach Mabry <zmabry2@illinois.edu>
2019-03-12 13:54:33 -0500

    Part 3

--- seddit-flask-19/app/routes.py
+++ seddit-flask-19/app/routes.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, flash, redirect, url_for
 from config import Config
 from app.forms import LoginForm, PostForm
+from flask_login import current_user, login_user, LoginManager, logout_user
+from app.models import User
+from app import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -30,10 +33,30 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
 
     if form.validate_on_submit():
-        flash('Welcome back to Seddit, u/{}!'.format(form.username.data))
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if not user:
+            user = User(username=form.username.data)
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            flash('Welcome to Seddit! Please log in with your new credentials')
+            return redirect(url_for('login'))
+
+        if not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        else:
+            flash('Welcome back to Seddit!')
+
+        login_user(user, remember=form.remember_me.data)
+
         return redirect(url_for('index'))
 
     return render_template('login.html', title='Sign In', form=form)
@@ -51,6 +74,22 @@ def newpost():
 def post(postnumber):
     return render_template('postpage.html', post=posts[int(postnumber)])
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 if __name__ == "__main__":
     app.secret_key = Config.SECRET_KEY
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    db.init_app(app)
+
     app.run(debug=True)
